275c9d3c6f6c59481cbd9b52465e0851b1da4db0
Emilio Casado Ortega <emicaor@gmail.com>
2021-05-13 17:41:50 +0200

    PRIMER COMMIT

---
+++ src/departamento.java
@@ -0,0 +1,41 @@
+import java.util.Vector;
+
+public class departamento {
+
+  public Integer codigo;
+
+  public String nombre;
+
+  public String localidad;
+
+    /**
+   * 
+   * @element-type Empleado
+   */
+  public Vector  trabajador;
+
+  public String getnombre() {
+  return null;
+  }
+
+  public String getlocalidad() {
+  return null;
+  }
+
+  public Integer getcodigo() {
+  return null;
+  }
+
+  public void setnombre(String nom) {
+  }
+
+  public void setcodigo(Integer cod) {
+  }
+
+  public void setlocalidad(String loc) {
+  }
+
+  public departamento() {
+  }
+
+}
---
+++ src/empleado.java
@@ -0,0 +1,56 @@
+import java.util.Vector;
+
+public class empleado {
+
+  public Integer codigo;
+
+  public String nombre;
+
+  public String localidad;
+
+  public Integer salario;
+
+    public empleado empleadojefe;
+    public departamento lugardetrabajo;
+    
+    /**
+     * 
+     * @element-type Empleado
+     */
+    public Vector  empleado;
+
+  public String getnombre() {
+  return null;
+  }
+
+  public String getlocalidad() {
+  return null;
+  }
+
+  public Integer getcodigo() {
+  return null;
+  }
+
+  public void setnombre(String nom) {
+  }
+
+  public void setcodigo(Integer cod) {
+  }
+
+  public void setlocalidad(String loc) {
+  }
+
+  public String getoficio() {
+  return null;
+  }
+
+  public void setoficio(String ofi) {
+  }
+
+  public void setsalario(Integer sal) {
+  }
+
+  public empleado() {
+  }
+
+}
